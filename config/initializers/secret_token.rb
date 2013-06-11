284911310e342b4ebca4f527e98200680ae471cb
robotzhang <445679586@qq.com>
2013-06-11 15:16:14 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialBuy::Application.config.secret_token = '9672bc4516d90323558b2f9d144a507c2c147b4a5a2666d39a7b29ce586e36aaab092bd0cadfb461b438e5a13146a12cac231779b686a58949ea7b5953773551'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+SocialBuy::Application.routes.draw do
+  root :to => 'application#index'
+end
