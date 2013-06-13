f126d51efc36f450a4e2ef753bc1021a5ca4ae3d
robotzhang <445679586@qq.com>
2013-06-13 09:40:58 +0800

    sina微博回调调通

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def new
 
   end
+
+  def create
+
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :weibo, "3095732433", "a9ce25c48fd330401cb31e18d875ae38"
+  provider :weibo, "3373872336", "61c1cad771b81df57ae88034dc9bb830", :client_options => {:ssl => {:verify => false}}
 end
