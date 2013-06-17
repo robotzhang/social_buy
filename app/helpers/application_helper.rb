de38cf55c3e618b13d57f03784729ed0839b27f3
robotzhang <445679586@qq.com>
2013-06-17 13:40:51 +0800

    nav highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_active(controller=nil, action=nil)
+    arr = controller.is_a?(Array) ? controller : [controller]
+    return ' class=active' if arr.include?(params[:controller]) && (action == params[:action] || action.blank?)
+    return ' class=active' if  action == 'index' && params[:controller] == 'application'
+    ''
+  end
 end
