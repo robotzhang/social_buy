6291318cc73e26fc30673b818ffaff322f0025cc
robotzhang <445679586@qq.com>
2013-06-20 10:29:21 +0800

    css

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,9 +1,10 @@
 class Business < ActiveRecord::Base
   attr_accessible :name
+  validate presence: true, uniqueness: {scope: :store_id}
   belongs_to :store
 
   def self.find_or_new_by_names(names)
-    businesses = self.where(name: names).all
+    businesses = self.where(name: names.uniq).all
     businesses ||= []
     (names - businesses.map{|b| b.name}).each do |name|
       businesses << Business.new(name: name)
