228a3bd270cd3ca5fbfc35c7eb01d1a27ff0e559
robotzhang <445679586@qq.com>
2013-06-26 15:10:04 +0800

    file uploaded

--- app/assets/javascripts/stores.js
+++ app/assets/javascripts/stores.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('a[data-toggle="tooltip"]').tooltip();
+    $('a[data-toggle="tooltip"]').tooltip({placement: 'top'});
 
     $('#btn_edit_store').popover({content: "dd", title: "编辑店铺", placement: 'bottom'});
 });
