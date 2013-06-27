168d00d5a370c733b1e3b6e4df5780e8b6659d48
robotzhang <445679586@qq.com>
2013-06-27 15:24:11 +0800

    联系方式维护完成

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,26 @@ class Contact < ActiveRecord::Base
 
   validates :provider, :way, presence: true, uniqueness: {scope: :user_id}
 
-  def self.providers
-    [['手机', 'mobile'], ['电话', 'phone'], ['QQ', 'qq'], ['人人', 'renren'], ['豆瓣', 'douban']]
+  def self.providers(contacts=[])
+    providers = [['手机', 'mobile'], ['电话', 'phone'], ['QQ', 'qq'], ['人人', 'renren'], ['豆瓣', 'douban']]
+    deletes = []
+    contacts.each do |contact|
+      providers.each do |provider|
+        if contact.provider == provider[1]
+          deletes << provider
+          break
+        end
+      end
+    end
+
+    providers - deletes
+  end
+
+  def provider_cn
+    Contact.providers.each do |p|
+      return p[0] if p[1] == self.provider
+    end
+
+    self.provider
   end
 end
