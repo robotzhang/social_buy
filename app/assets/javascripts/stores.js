b192de8602e9efa33d65b80605f40aa801b9299a
robotzhang <445679586@qq.com>
2013-06-27 19:09:56 +0800

    loading edit store form

--- app/assets/javascripts/stores.js
+++ app/assets/javascripts/stores.js
@@ -3,17 +3,19 @@ $(document).ready(function() {
 
     $('#btn_edit_store').click(function(){
         $this = $(this);
-        $.get($this.data('href'), function(html){
-            var panel = $('#panel_edit_store');
-            if ($.trim(panel.find('.content').html()) != '') {
-                panel.toggle();
-            } else {
+        var panel = $('#panel_edit_store');
+        if (panel.find('.loading').length == 0) {
+            panel.toggle();
+        } else {
+            panel.css({top: $this.offset().top + $this.outerHeight() + 10}).show();
+            $.get($this.data('href'), function(html){
                 panel.find('.content').html(html);
-                panel.css({left: $this.offset().left, top: $this.offset().top + $this.outerHeight() + 10})
+                panel.css({left: $this.offset().left - panel.width() / 2 + $this.width() / 2})
                     .show();
-            }
-        });
-
+                $('a[data-toggle="tooltip"]').tooltip({placement: 'top'});
+                panel.find('.loading').remove();
+            });
+        }
         return false;
     });
 });
